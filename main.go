da29c32098b0d6818d9d9389c02b567225277b88
tagasouichiro <stitow14@gmail.com>
2017-05-18 22:49:25 +0900

    Add int sort

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	nums := []int{4, 3, 2, 10, 8}
+	sort.Ints(nums)
+	fmt.Println(nums)
+	// check sorted
+	fmt.Println(sort.IntsAreSorted(nums))
+}
